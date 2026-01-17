4b9b4fd260c6b74f363d0353a164656265402c9d
Thisara kavishka <thisarakavishka0@gmail.com>
2026-01-17 09:55:51 +0530

    Feat: add icons for Icon component

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -1,6 +1,18 @@
-import { Home, Calendar, Dumbbell, User, Utensils } from "lucide-react"; 
+import {
+  Library,
+  History,
+  Home,
+  Calendar,
+  Dumbbell,
+  User,
+  List,
+  Utensils,
+} from "lucide-react";
 
 export const Icons = {
+  History,
+  Library,
+  List,
   Home,
   Schedule: Calendar,
   Workout: Dumbbell,
